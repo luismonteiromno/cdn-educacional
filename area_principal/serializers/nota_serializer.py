580bea5f7a2333da6af52c81a47ff60fb5257a77
Luis Monteiro <109322475+luismonteiromno@users.noreply.github.com>
2025-05-03 11:48:14 -0300

    Feat/campos populados nos serializers (#3)
    * feat: Novos filtros adicionados na API de professores
    
    - Filtros de `turmas` e `disciplinas` foram adicionados na API de Professores;
    
    * feat: Campos populados nos serializers de prova e nota

--- area_principal/serializers/nota_serializer.py
+++ area_principal/serializers/nota_serializer.py
@@ -3,8 +3,16 @@ from rest_framework import serializers
 
 from ..models import Nota
 
+from .aluno_serializer import AlunoSerializer
+from .professor_serializer import ProfessorSerializer
+from .prova_serializer import ProvaSerializer
+
 
 class NotaSerializer(serializers.ModelSerializer):
+    aluno = AlunoSerializer(read_only=True)
+    professor = ProfessorSerializer(read_only=True)
+    prova_feita = ProvaSerializer(read_only=True)
+    
     class Meta:
         model = Nota
         fields = '__all__'
--- area_principal/serializers/prova_serializer.py
+++ area_principal/serializers/prova_serializer.py
@@ -3,8 +3,15 @@ from rest_framework import serializers
 
 from ..models import Prova
 
+from .turma_serializer import TurmaSerializer
+from .disciplina_serializer import DisciplinaSerializer
+from .professor_serializer import ProfessorSerializer
 
 class ProvaSerializer(serializers.ModelSerializer):
+    turma = TurmaSerializer(read_only=True)
+    disciplina = DisciplinaSerializer(read_only=True)
+    professor = ProfessorSerializer(read_only=True)
+    
     class Meta:
         model = Prova
         fields = '__all__'
