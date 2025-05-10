e76f8d36d36f4ce1fabc9433e82f70ea5adc0ee6
Luis Monteiro <luismonteirodeveloper@gmail.com>
2025-05-10 19:01:11 -0300

    fix: Correção no serializer
    - Havia muita informação redundante no serializers de frequência dos alunos;

--- area_aluno/serializers/frequencia_aluno_serializer.py
+++ area_aluno/serializers/frequencia_aluno_serializer.py
@@ -7,9 +7,9 @@ from area_principal.serializers import TurmaSerializer, AlunoSerializer, Profess
 
 
 class FrequenciaAlunoSerializer(serializers.ModelSerializer):
-    alunos = AlunoSerializer(many=True, read_only=True)
+    # alunos = AlunoSerializer(many=True, read_only=True)
     turma = TurmaSerializer(read_only=True)
-    professor = ProfessorSerializer(read_only=True)
+    # professor = ProfessorSerializer(read_only=True)
     
     class Meta:
         model = FrequenciaAluno
